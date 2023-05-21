aad612d37413515915ac1adaf2cdc11c3e7dc236
Theerakarn <theerakarn.maiwong@accenture.com>
2023-05-21 18:11:30 +0700

    feat: :tada: init

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,7 +1,6 @@
 import { component$, Slot, useStyles$ } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
 
-import Header from '~/components/starter/header/header';
 import Footer from '~/components/starter/footer/footer';
 
 import styles from './styles.css?inline';
@@ -16,11 +15,10 @@ export default component$(() => {
   useStyles$(styles);
   return (
     <>
-      <Header />
       <main>
         <Slot />
+        <Footer />
       </main>
-      <Footer />
     </>
   );
 });
